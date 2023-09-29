51d784666b6ae65d5fb6d43e79f8cc536b41ab34
Stephen Adams <stephen.adams@redhat.com>
2023-09-29 16:50:03 -0400

    Add github link
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

--- frontend/src/HeadContent.svelte
+++ frontend/src/HeadContent.svelte
@@ -1,7 +1,13 @@
 <script>
-import { Header } from '@svelteuidev/core'; 
+import { Header, Flex, Tooltip, Button } from '@svelteuidev/core'; 
+import { GithubLogo } from 'radix-icons-svelte';
 </script>
 
 <Header>
+<Flex justify="space-between">
 <a href="/" class="header-link"><h1>Poller Bear</h1></a>
+<Tooltip label="Github">
+<Button href="https://www.github.com/SteveHNH/poller-bear"><GithubLogo /></Button>
+</Tooltip>
+</Flex>
 </Header> 
