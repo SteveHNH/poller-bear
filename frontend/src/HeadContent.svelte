f24f44957d1543f6e649bdce548ca544c3500c69
Stephen Adams <stephen.adams@redhat.com>
2023-09-29 16:58:37 -0400

    Center the github button in the header
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

--- frontend/src/HeadContent.svelte
+++ frontend/src/HeadContent.svelte
@@ -1,13 +1,15 @@
 <script>
-import { Header, Flex, Tooltip, Button } from '@svelteuidev/core'; 
+import { Center, Header, Flex, Tooltip, Button } from '@svelteuidev/core'; 
 import { GithubLogo } from 'radix-icons-svelte';
 </script>
 
 <Header>
 <Flex justify="space-between">
 <a href="/" class="header-link"><h1>Poller Bear</h1></a>
+<Center inline>
 <Tooltip label="Github">
 <Button href="https://www.github.com/SteveHNH/poller-bear"><GithubLogo /></Button>
 </Tooltip>
+</Center>
 </Flex>
 </Header> 
