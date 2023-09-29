6827d900c46a23476d76da781a6db32ae4419740
Stephen Adams <stephen.adams@redhat.com>
2023-09-29 15:14:50 -0400

    Update GUI
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

--- frontend/src/CreatePoll.svelte
+++ frontend/src/CreatePoll.svelte
@@ -5,13 +5,18 @@ import { Button, Input } from '@svelteuidev/core';
     let question = "";
     let responses = ["", ""];
 
+    $: if (responses[responses.length - 1]) {
+      responses = [...responses, ""];
+    }
+
     async function handleSubmit() {
+        const validOptions = responses.filter(response => Boolean(response.trim()));
         const response = await fetch("/api/create", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({ question, responses: responses.map(text => ({ text }))})
+            body: JSON.stringify({ question, responses: validOptions.map(text => ({ text }))})
         });
 
         const data = await response.json();
@@ -19,14 +24,6 @@ import { Button, Input } from '@svelteuidev/core';
         navigate(`/polls/${data.id}`)
     }
 
-    async function addOption() {
-      responses = [...responses, ""];
-      console.log(responses)
-    }
-
-    async function removeOption(indexToRemove) {
-      responses = responses.filter((_, index) => index !== indexToRemove);
-    }
 </script>
 
 
@@ -35,10 +32,11 @@ import { Button, Input } from '@svelteuidev/core';
 <ul>
     {#each responses as _, index (index)}
       <div class="response-container">
-        <li><Input bind:value={responses[index]} placeholder={`Option ${index + 1}`} />{#if index === (responses.length - 1) && responses.length > 2}<Button type="button" on:click={() => removeOption(index)}>-</Button>{/if}</li>
+        <li>
+        <Input bind:value={responses[index]} placeholder={`Option ${index + 1}`} />
+        </li>
       </div>
     {/each}
-    <Button type="button" on:click={addOption}>+</Button>
 </ul>
 <Button type="submit">Create Poll</Button>
 </form>
